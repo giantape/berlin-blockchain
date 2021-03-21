9f35bb687bd89bf2ae650267b19c13deac6e2227
g14n4p3 <g14n4p3@g14n4p3s-MBP.fritz.box>
2021-03-21 22:38:46 +0100

    first commit

---
+++ app/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Blockchain = require('../blockchain');
+const P2pServer = require('./p2p-server');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
+
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
+
+const app = express();
+const bc = new Blockchain();
+const wallet = new Wallet();
+const tp = new TransactionPool();
+const p2pServer = new P2pServer(bc, tp);
+
+app.use(bodyParser.json());
+
+app.get('/blocks', (req, res) => {
+    res.json(bc.chain);
+});
+
+app.post('/mine', (req, res) => {
+    const block = bc.addBlock(req.body.data);
+    console.log(`New block added: ${block.toString}`);
+
+    p2pServer.syncChains();
+    res.redirect('/blocks');
+});
+
+app.get('/transactions', (req, res) => {
+    res.json(tp.transactions);
+});
+
+app.post('/transact', (req, res) => {
+    const { recipient, amount } = req.body;
+    const transacation = wallet.createTransaction(recipient, amount, tp);
+    p2pServer.broadcastTransaction(transacation);
+    res.redirect('/transactions');
+});
+
+app.get('/public-key', (req, res) => {
+    res.json({ publicKey: wallet.publicKey });
+})
+app.listen(HTTP_PORT, () => console.log(`Listen on Port ${HTTP_PORT}`))
+p2pServer.listen();
